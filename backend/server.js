24372f4139b9d00524e5a834218f2ab811d5614b
Davi <davinunesbrazil2003@gmail.com>
2026-05-21 20:21:33 -0300

    feat: servidor Express e Firebase configurados

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 dotenv.config();
+const { db } = require("./src/firebase");
+console.log("Firebase conectado!");
 
 const app = express();
 
